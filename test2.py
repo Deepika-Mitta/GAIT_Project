1dd5bae556cc04e235fd14369bfc26dc44dc4392
Deepika-Mitta <107551056+Deepika-Mitta@users.noreply.github.com>
2024-12-04 01:34:34 -0600

    added LLAMA model for suggestions

--- test2.py
+++ test2.py
@@ -11,52 +11,8 @@ class ASLDetector:
             min_detection_confidence=0.7
         )
         self.mp_draw = mp.solutions.drawing_utils
-
-    def get_finger_states(self, landmarks):
-        """Returns the state of each finger (extended or not)"""
-        points = np.array([[lm.x, lm.y, lm.z] for lm in landmarks.landmark])
-        
-        # Get states for each finger (thumb handled separately)
-        states = []
-        
-        # Thumb (comparing tip to base)
-        thumb_extended = points[4][0] < points[3][0]  # For right hand
-        states.append(thumb_extended)
-        
-        # Other fingers
-        for finger_base in [5, 9, 13, 17]:  # Index, middle, ring, pinky
-            finger_tip = finger_base + 3
-            finger_extended = points[finger_tip][1] < points[finger_base][1]
-            states.append(finger_extended)
-            
-        return states
-
-    def detect_letter(self, landmarks):
-        if landmarks is None:
-            return None
-            
-        states = self.get_finger_states(landmarks)
-        points = np.array([[lm.x, lm.y, lm.z] for lm in landmarks.landmark])
-        
-        # Define letter patterns
-        if not any(states[1:]) and not states[0]:  # All fingers closed
-            return 'A'
-            
-        if all(states[1:]) and not states[0]:  # All fingers extended except thumb
-            return 'B'
-            
-        if states[1] and not any(states[2:]) and not states[0]:  # Only index extended
-            return 'D'
-            
-        if all(states):  # All fingers extended
-            return 'E'
-            
-        # Add distance calculations for letters that need specific positioning
-        distance_index_thumb = np.linalg.norm(points[4] - points[8])
-        if distance_index_thumb < 0.05 and not any(states[2:]):  # Thumb and index touching
-            return 'F'
-            
-        return None
+        self.landmarks = None  # Add this line
+        self.current_letter = None  # Add this line
 
     def process_frame(self, frame):
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -64,6 +20,7 @@ class ASLDetector:
         
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
+                self.landmarks = hand_landmarks  # Store landmarks
                 self.mp_draw.draw_landmarks(
                     frame, 
                     hand_landmarks, 
@@ -83,20 +40,65 @@ class ASLDetector:
                         1
                     )
                 
-                letter = self.detect_letter(hand_landmarks)
-                if letter:
+                self.current_letter = self.detect_letter(hand_landmarks)  # Store current letter
+                if self.current_letter:
                     cv2.putText(
                         frame,
-                        f"Letter: {letter}",
+                        f"Letter: {self.current_letter}",
                         (10, 200),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         2,
                         (0, 255, 0),
                         2
                     )
+        else:
+            self.landmarks = None  # Reset when no hands detected
+            self.current_letter = None
         
         return frame
 
+    # Rest of the methods remain the same
+    def get_finger_states(self, landmarks):
+        """Returns the state of each finger (extended or not)"""
+        points = np.array([[lm.x, lm.y, lm.z] for lm in landmarks.landmark])
+        
+        states = []
+        
+        thumb_extended = points[4][0] < points[3][0]
+        states.append(thumb_extended)
+        
+        for finger_base in [5, 9, 13, 17]:
+            finger_tip = finger_base + 3
+            finger_extended = points[finger_tip][1] < points[finger_base][1]
+            states.append(finger_extended)
+            
+        return states
+
+    def detect_letter(self, landmarks):
+        if landmarks is None:
+            return None
+            
+        states = self.get_finger_states(landmarks)
+        points = np.array([[lm.x, lm.y, lm.z] for lm in landmarks.landmark])
+        
+        if not any(states[1:]) and not states[0]:
+            return 'A'
+            
+        if all(states[1:]) and not states[0]:
+            return 'B'
+            
+        if states[1] and not any(states[2:]) and not states[0]:
+            return 'D'
+            
+        if all(states):
+            return 'E'
+            
+        distance_index_thumb = np.linalg.norm(points[4] - points[8])
+        if distance_index_thumb < 0.05 and not any(states[2:]):
+            return 'F'
+            
+        return None
+    
 def main():
     detector = ASLDetector()
     cap = cv2.VideoCapture(0)
